5f95d59f0bdb4c0afd266a9be3fb54e1ec0e2aa3
Shea Stewart <sheastewart@sheasus.local>
2018-10-04 14:37:24 -0400

    update run.sh

--- jenkins/openshift/jenkins2-with-docker/bin/run.sh
+++ jenkins/openshift/jenkins2-with-docker/bin/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-mkdir /var/lib/jenkins/groovy.init.d/
-cp /groovy/* /var/lib/jenkins/groovy.init.d/
+mkdir /var/lib/jenkins/init.groovy.d/
+cp /groovy/* /var/lib/jenkins/init.groovy.d/
+chmod 755 /var/lib/jenkins/init.groovy.d/*
+
 sh /usr/libexec/s2i/run
